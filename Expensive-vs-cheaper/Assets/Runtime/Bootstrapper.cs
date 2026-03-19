8eb1e163dc061a0a13caf5bdffed15ec8004995c
Maxim S. <its3zin.main@gmail.com>
2026-03-19 17:08:20 +0300

    setup project

---
+++ Expensive-vs-cheaper/Assets/Runtime/Bootstrapper.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace DoubleB.Runtime
+{
+    public class Bootstrapper : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
