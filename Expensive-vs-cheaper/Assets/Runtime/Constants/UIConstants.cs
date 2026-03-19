bddf827eee6db329e5efd7c6c805a49c432ea4e9
Maxim S. <its3zin.main@gmail.com>
2026-03-19 23:45:20 +0300

    add: ui constants

--- Expensive-vs-cheaper/Assets/Runtime/Constants/UIConstants.cs
+++ Expensive-vs-cheaper/Assets/Runtime/Constants/UIConstants.cs
@@ -6,5 +6,9 @@ namespace DoubleB.Runtime
         public const string Gameplay = "gameplay";
         public const string Root = "root";
         public const string StartButton = "start-button";
+        public const string MoreExpensiveButton = "more-expensive-button";
+        public const string CheaperButton = "cheaper-button";
+        public const string Item = "item";
+        public const string Content = "content";
     }
 }
